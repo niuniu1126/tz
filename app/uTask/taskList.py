751355ede8f94cffd7973bfe29d5dd8693c71da7
Eric <i_chaochao@hotmail.com>
2020-03-02 16:50:26 +0800

    celery 任务流 未完成

--- app/uTask/taskList.py
+++ app/uTask/taskList.py
@@ -15,25 +15,25 @@ def stock_base_stock_update(self):
     return flag
 
 @celery.task(bind=True, max_retries=3, default_retry_delay=1 * 6)
-def stock_base_stock_daily_data(self):
+def stock_base_stock_daily_data():
     """各股每日指标"""
     try:
         flag_d = initData.init_stock_daily_data()
     except Exception as e:
-        raise self.retry(exc=e, countdown=60)
+        raise stock_base_stock_daily_data.retry(exc=e, countdown=60)
     return flag_d
 
 @celery.task(bind=True, max_retries=3, default_retry_delay=1 * 6)
-def stock_base_stock_fi_data(self):
+def stock_base_stock_fi_data():
     """各股基本财务数据"""
     try:
         flag_f = initData.init_stock_fi_data()  # 个股基本情况 由于接口取值通信问题 暂不更新
     except Exception as e:
-        raise self.retry(exc=e, countdown=60)
+        raise stock_base_stock_fi_data.retry(exc=e, countdown=60)
     return flag_f
 
 @celery.task(bind=True, max_retries=3, default_retry_delay=1 * 6)
-def stock_base_stock_user_data(self):
+def stock_base_stock_user_data():
     """用户使用股票数据"""
     flag_u = initData.init_stock_daily_user_details()  # 用户使用股票数据
     return flag_u
--- app/uTask/taskRun.py
+++ app/uTask/taskRun.py
@@ -1,4 +1,9 @@
+import time
+
 from app.uTask import taskList
+from celery.result import AsyncResult
+from celery import Celery, chain, group
+
 
 # 启动celery
 # celery -A app.uTask.taskList worker -l info -P eventlet
@@ -9,14 +14,17 @@ from app.uTask import taskList
 # TODO 初始化更新沪深股票
 def stock_base_task():
     """沪深股市-初始化股票信息"""
-    tasks_id = taskList.stock_base_stock_update.delay()
+    tasks_id = taskList.stock_base_stock_update.AsyncResult()
     return tasks_id
 
 # TODO 更新各股每日指标 每日更新
 def stock_daily_data_task():
     """各股每日指标"""
-    tasks_id = taskList.stock_base_stock_daily_data.delay()
-    return tasks_id
+    # tasks_id = taskList.stock_base_stock_daily_data.delay()
+    # result = taskList.stock_base_stock_daily_data.apply_async(args=(), link=taskList.stock_base_stock_user_data.s())
+    result = chain(taskList.stock_base_stock_daily_data.s() | taskList.stock_base_stock_user_data.s())()
+    # return tasks_id
+    return result
 
 # TODO 更新各股财务数据 每日更新
 def stock_fi_data_task():
@@ -31,7 +39,27 @@ def stock_user_data_task():
     return tasks_id
 
 if __name__ == '__main__':
-    r = stock_base_task()
-    # print(r)
-    r1 = stock_fi_data_task()
-    print(r1)
+    # r1 = stock_base_task()
+    r2 = stock_daily_data_task()
+    # # r3 = stock_fi_data_task()
+    # while 1:
+    #     finished = r2.ready()
+    #     print(finished)
+    #     time.sleep(2)
+    print(r2)
+    # res = AsyncResult(r2)
+    # res = group(stock_daily_data_task(), stock_user_data_task())()
+    # if res.successful():
+    #     while 1:
+    #         try:
+    #
+    #             if res.status == 'SUCCESS':
+    #                 r4 = stock_user_data_task()
+    #                 break
+    #             if res.result == 'False':
+    #                 break
+    #         except Exception as e:
+    #             time.sleep(1)
+
+    # print(res.status, '----', res.result)
+    # print(res)
